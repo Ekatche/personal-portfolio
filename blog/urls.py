1238c4bc67d1658c55fef67bf5676e9f63f1762f
Ekatche <atchokatche@gmail.com>
2022-03-11 13:01:29 +0100

    add my portfolio project

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib import admin
+from . import views
+
+app_name ='blog'
+
+urlpatterns = [
+    path('',views.all_blogs, name = 'all_blogs'),
+    path('<int:blog_id>/',views.details, name = 'detail'),
+
+]
+
